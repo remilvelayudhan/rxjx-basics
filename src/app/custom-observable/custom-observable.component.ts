a66275129d9a46589d20f70faae6dd7c3813af31
remil <remil.velayudhan@gmail.com>
2022-11-26 15:40:35 +0530

    custom observer

--- src/app/custom-observable/custom-observable.component.ts
+++ src/app/custom-observable/custom-observable.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-custom-observable',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomObservableComponent implements OnInit {
 
+  newObservable = new Observable<number>((observer) => {
+    // observer.next("data1"),
+    // observer.next("data2"),
+    // observer.next("data3"),
+    // observer.next("data4"),
+    // observer.next("data5"),
+
+    // for(let i=0;i<10;i++){
+    //   observer.next("data1")
+    // }
+  
+    setTimeout(() => {
+      for (let i = 0; i < 10; i++)
+        observer.next(i);
+    },  3000);
+
+    // observer.complete();
+
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+    this.newObservable.subscribe({
+      next: (data) => { console.log(data) },
+      error: (error) => { console.log(error) },
+      complete: () => { console.log("custom observer send sucessfully") },
+    });
   }
 
 }
